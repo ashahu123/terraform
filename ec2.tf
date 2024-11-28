8b1615f4f9a31ae3b84c3d4d2b50c4f0f4cda3af
ashahu123 <aniketshahu004@gmail.com>
2024-11-28 09:39:02 +0530

    new commit

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,10 @@
 provider "aws" {
     region = "ap-south-1"
 }
+
+resource "aws_instance" "instance1" {
+  ami = "ami-0dee22c13ea7a9a67"
+  instance_type = "t2.micro"
+  keyname      = "docker"
+  security_group = ["default"]
+}
